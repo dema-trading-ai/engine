e8c93af84f65e944acdfe47e793ab507c7e2a115
Tijs Verbeek <tijsverbeek@gmail.com>
2021-03-10 12:16:34 +0100

    [Fix] Pep256 try fix

--- utils.py
+++ utils.py
@@ -7,23 +7,21 @@ def get_project_root():
 
 
 def default_empty_array_dict() -> list:
-    """:return: list for initializing dictionary :rtype: List"""
+    """:return: list for initializing dictionary :rtype: List."""
     return []
 
 
 def default_empty_dict_dict() -> dict:
-    """:return: Dictionary for initializing default dictionary :rtype: Dict"""
+    """:return: Dictionary for initializing default dictionary :rtype: Dict."""
     return defaultdict(int)
 
 
 def calculate_worth_of_open_trades(open_trades) -> float:
-    """
-    Method calculates worth of open trades
-
+    """Method calculates worth of open trades
     :param open_trades: array of open trades
     :type open_trades: [Trade]
     :return: returns the total value of all open trades
-    :rtype: float
+    :rtype: float.
     """
     return_value = 0
     for trade in open_trades:
