b0b2197b38eda947d83ff5df089e4a1f92d5c300
noudcorten <NRMCorten@gmail.com>
2021-04-20 20:06:51 +0200

    [Feat] Fixed codacy issues

--- models/trade.py
+++ models/trade.py
@@ -97,7 +97,7 @@ class Trade:
             else:
                 self.sl_type = 'standard'   # when dynamic not configured use normal stoploss
         elif self.sl_type == 'standard':
-            sl_price = self.open - (self.open * (abs(self.sl_perc) / 100))
+            self.sl_price = self.open - (self.open * (abs(self.sl_perc) / 100))
         elif self.sl_type == 'trailing':
             self.sl_sell_time, self.sl_price = self.trailing_stoploss(data_dict, ohlcv['time'])
 
