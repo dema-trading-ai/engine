c8a37e282941ba115497026ef5b711d61415b69d
geordle <georditrapman@gmail.com>
2021-05-22 18:16:53 +0200

    Add talib as import

--- hooks/hook-talib.py
+++
@@ -1,6 +0,0 @@
-from PyInstaller.utils.hooks import collect_submodules
-from PyInstaller.utils.hooks import collect_data_files
-
-hiddenimports = collect_submodules('talib')
-
-datas = collect_data_files('talib')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cli.arg_parse import execute_for_args
 from cli.checks.latest_version import print_warning_if_version_outdated
 from cli.prepare_workspace import prepare_workspace
 from main_controller import MainController
+import talib.abstract as ta
 
 
 # TODO add .gitignore file to setup resources dir
