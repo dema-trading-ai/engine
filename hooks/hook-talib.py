c329044bf93a24f39c7a01ffff9865d76105cafd
geordle <georditrapman@gmail.com>
2021-05-22 18:31:19 +0200

    Add talib as import

--- hooks/hook-talib.py
+++ hooks/hook-talib.py
@@ -2,5 +2,4 @@ from PyInstaller.utils.hooks import collect_submodules
 from PyInstaller.utils.hooks import collect_data_files
 
 hiddenimports = collect_submodules('talib')
-
 datas = collect_data_files('talib')
