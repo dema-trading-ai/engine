22c5ecd77089b594ad5498fcc85d39a868f656d5
Sherida van den Bent <sheridavandenbent@gmail.com>
2021-06-03 13:47:42 +0200

    Open Trades: Profit Percentage Tests

--- test/stats/test_open_trades.py
+++ test/stats/test_open_trades.py
@@ -47,3 +47,35 @@ def test_open_trades_profit_percentage_negative():
 
     # Assert
     assert stats.open_trade_res[0].curr_profit_percentage == -50.5
+
+
+def test_open_trades_profit_positive():
+    """Given a profiting left open trade, profit should be correct, including buy fee"""
+    # Arrange
+    fixture = StatsFixture(['COIN/BASE'])
+
+    fixture.frame_with_signals['COIN/BASE'] \
+        .add_entry(open=1, high=1, low=1, close=1, volume=1, buy=1, sell=0) \
+        .add_entry(open=1, high=2, low=1, close=2, volume=1, buy=0, sell=0)
+
+    # Act
+    stats = fixture.create().analyze()
+
+    # Assert
+    assert stats.open_trade_res[0].curr_profit == 98
+
+
+def test_open_trades_profit_negative():
+    """Given a losing left open trade, profit should be correct, including buy fee"""
+    # Arrange
+    fixture = StatsFixture(['COIN/BASE'])
+
+    fixture.frame_with_signals['COIN/BASE'] \
+        .add_entry(open=2, high=2, low=2, close=2, volume=1, buy=1, sell=0) \
+        .add_entry(open=2, high=2, low=1, close=1, volume=1, buy=0, sell=0)
+
+    # Act
+    stats = fixture.create().analyze()
+
+    # Assert
+    assert stats.open_trade_res[0].curr_profit == -50.5
