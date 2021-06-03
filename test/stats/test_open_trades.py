dd207817f7517f1422fd4bcc0caf26386e6fe7f4
Sherida van den Bent <sheridavandenbent@gmail.com>
2021-06-03 15:06:28 +0200

    test open trades

--- test/stats/test_open_trades.py
+++ test/stats/test_open_trades.py
@@ -1,4 +1,5 @@
 import math
+from datetime import datetime
 
 from test.stats.stats_test_utils import StatsFixture
 
@@ -113,3 +114,19 @@ def test_open_trades_drawdown_negative():
 
     # Assert
     assert stats.open_trade_res[0].max_seen_drawdown == -50.5
+
+
+def test_open_trades_opened_at():
+    """Given a left open trade, opened_at should be timestep 1"""
+    # Arrange
+    fixture = StatsFixture(['COIN/BASE'])
+
+    fixture.frame_with_signals['COIN/BASE'] \
+        .add_entry(open=2, high=2, low=2, close=2, volume=1, buy=1, sell=0) \
+        .add_entry(open=2, high=2, low=1, close=1, volume=1, buy=0, sell=0)
+
+    # Act
+    stats = fixture.create().analyze()
+
+    # Assert
+    assert stats.open_trade_res[0].opened_at.timestamp() == 0
