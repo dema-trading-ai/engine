e7323d1ca58f0f3ce2ce8c20c99d0e1658a34c33
geordle <georditrapman@gmail.com>
2021-06-15 15:00:33 +0200

    Made downloading of ohlcv concurrent

--- main_controller.py
+++ main_controller.py
@@ -1,6 +1,6 @@
 # Files
 from modules.output import OutputModule
-from modules.setup.config import ConfigModule
+from modules.setup.config import ConfigModule, create_config_module
 from modules.setup import SetupModule
 from modules.stats.stats import StatsModule
 from modules.stats.stats_config import to_stats_config
@@ -10,20 +10,19 @@ from modules.stats.tradingmodule_config import create_trading_module_config
 
 class MainController:
 
-    def __init__(self, args):
-        self.config = ConfigModule(args)
-        self.setup_module = SetupModule(self.config)
+    async def run(self, args) -> None:
+        async with create_config_module(args) as config:
+            setup_module = SetupModule(config)
 
-    def run(self) -> None:
-        algo_module = self.setup_module.setup()
-        df, dict_with_signals = algo_module.run()
+            algo_module = await setup_module.setup()
+            df, dict_with_signals = algo_module.run()
 
-        stats_config = to_stats_config(self.config)
+            stats_config = to_stats_config(config)
 
-        trading_module_config = create_trading_module_config(self.config)
-        trading_module = TradingModule(trading_module_config)
-        stats_module = StatsModule(stats_config, dict_with_signals, trading_module, df)
+            trading_module_config = create_trading_module_config(config)
+            trading_module = TradingModule(trading_module_config)
+            stats_module = StatsModule(stats_config, dict_with_signals, trading_module, df)
 
-        stats = stats_module.analyze()
+            stats = stats_module.analyze()
 
-        OutputModule(stats_config).output(stats)
+            OutputModule(stats_config).output(stats)
--- modules/setup/__init__.py
+++ modules/setup/__init__.py
@@ -9,9 +9,10 @@ class SetupModule(object):
     def __init__(self, config_module: ConfigModule):
         self.config = config_module
 
-    def setup(self) -> AlgoModule:
+    async def setup(self) -> AlgoModule:
         print_pairs(self.config.raw_config)  # TODO fix mixed level of abstraction
-        ohlcv_pair_frames = DataModule(self.config).load_historical_data()
+        data_module = await DataModule.create(self.config)
+        ohlcv_pair_frames = await data_module.load_historical_data()
 
         strategy = load_strategy_from_config(self.config.strategy_definition)
         backtesting_module = BackTesting(ohlcv_pair_frames, self.config, strategy)
