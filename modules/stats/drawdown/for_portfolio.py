4d40e042c76ac0d3a6c581812936f4be8b8eff28
Sherida van den Bent <sheridavandenbent@gmail.com>
2021-06-18 15:19:07 +0200

    consecutive losses

--- modules/stats/drawdown/for_portfolio.py
+++ modules/stats/drawdown/for_portfolio.py
@@ -15,6 +15,7 @@ def get_max_seen_drawdown_for_portfolio(capital_per_timestamp: dict):
     max_seen_drawdown["drawdown"] = df["drawdown"].min()
     max_seen_drawdown["at"] = df["drawdown"].idxmin()
     max_seen_drawdown["from"] = df.loc[:max_seen_drawdown["at"]].value.idxmax()
+
     max_seen_drawdown["to"] = df.loc[max_seen_drawdown["at"]:].drawdown.eq(0.0).idxmax()
 
     return max_seen_drawdown
@@ -31,7 +32,10 @@ def get_max_realised_drawdown_for_portfolio(realised_profits_per_timestamp: dict
     df["drawdown"] = (df["value"] - df["value"].cummax()) / df["value"].cummax()
     df['losing_trade'] = df.value.lt(df.value.shift())
 
-    df['consecutive_losses'] = df.losing_trade.cumsum()-df.losing_trade.cumsum().where(~df.losing_trade).ffill().fillna(0).astype(int)
+    df['consecutive_losses'] = df.losing_trade.cumsum()-df.losing_trade.cumsum().where(~df.losing_trade)\
+        .ffill()\
+        .fillna(0)\
+        .astype(int)
 
     max_realised_drawdown['drawdown'] = df["drawdown"].min()
     max_realised_drawdown['losing_trades'] = df["losing_trade"].value_counts().loc[True]
