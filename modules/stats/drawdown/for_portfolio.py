5c54ee2d7b02b009e4b5e7f9a84f2f00025c7a56
Sherida van den Bent <sheridavandenbent@gmail.com>
2021-06-21 17:04:13 +0200

    ratio max drawdown

--- modules/stats/drawdown/for_portfolio.py
+++ modules/stats/drawdown/for_portfolio.py
@@ -10,7 +10,7 @@ def get_max_seen_drawdown_for_portfolio(capital_per_timestamp: dict):
     }
 
     df = pd.DataFrame.from_dict(capital_per_timestamp, columns=['value'], orient='index')
-    df["drawdown"] = (df["value"] - df["value"].cummax()) / df["value"].cummax()
+    df["drawdown"] = df["value"] / df["value"].cummax()
 
     max_seen_drawdown["drawdown"] = df["drawdown"].min()
     max_seen_drawdown["at"] = df["drawdown"].idxmin()
