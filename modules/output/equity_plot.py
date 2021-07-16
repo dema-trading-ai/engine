7ae12d108132178c067fb738b6f7c3abbeee7d8b
deborahvans <71016477+deborahvans@users.noreply.github.com>
2021-07-16 14:28:01 +0200

    min-max

--- modules/output/equity_plot.py
+++ modules/output/equity_plot.py
@@ -15,11 +15,11 @@ def equity_plot(tradingmodule):
 
     # get dates and y range
     dates = [datetime.fromtimestamp(time / 1000) for time in df[0]]
-    min = int(df[1].min() - 10)
-    max = int(df[1].max() + 10)
+    min_value = int(df[1].min() - 10)
+    max_value = int(df[1].max() + 10)
 
     # create figure
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=dates, y=df[1], fill='tozeroy'))  # fill down to xaxis
-    fig.update_yaxes(range=[min, max])
+    fig.update_yaxes(range=[min_value, max_value])
     fig.write_html("data/backtesting-data/plots/equity/equityplot.html", auto_open=False)
