e747d90e7f673bd57a03979b0cdc0661b86cfc74
deborahvans <71016477+deborahvans@users.noreply.github.com>
2021-07-16 13:43:54 +0200

    equityplot.py

---
+++ modules/output/equity_plot.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from pathlib import Path
+import pandas as pd
+
+from plotly import graph_objects as go
+
+def equity_plot(tradingmodule):
+    Path("data/backtesting-data/plots/equity").mkdir(parents=True, exist_ok=True)
+    df = pd.DataFrame(list(tradingmodule.capital_per_timestamp.items()))
+
+    # remove first value since its 0
+    df.drop(index=df.index[0],
+            axis=0,
+            inplace=True)
+
+    # get dates and y range
+    dates = [datetime.fromtimestamp(time / 1000) for time in df[0]]
+    min = int(df[1].min() - 10)
+    max = int(df[1].max() + 10)
+
+    # create figure
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=dates, y=df[1], fill='tozeroy'))  # fill down to xaxis
+    fig.update_yaxes(range=[min, max])
+    fig.write_html("data/backtesting-data/plots/equity/equityplot.html", auto_open=False)
