8c3097fcaee40af10aec36ea132e4da3a206045b
noudcorten <NRMCorten@gmail.com>
2021-07-19 11:27:27 +0200

    edit prints in windows executable

--- installation_helpers/install-windows.ps1
+++ installation_helpers/install-windows.ps1
@@ -1,35 +1,29 @@
 $ErrorActionPreference = "Stop"
 $ProgressPreference = 'SilentlyContinue'
-
 function IsElevated {
     $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
     $p = New-Object System.Security.Principal.WindowsPrincipal($id)
     if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
-    { Write-Output $true }      
+    { Write-Output $true }
     else
-    { Write-Output $false }   
+    { Write-Output $false }
 }
-
 if (-not(IsElevated)) {
-    throw "Please run this script as administrator" 
+    throw "Please run this script as administrator"
 }
-
+echo "Installing DemaTrading.ai 'engine'... (this takes +/- 10 seconds")
 $executableUri = "https://engine-store.ams3.digitaloceanspaces.com/engine-windows-stable.zip"
 $tempDir = $env:TEMP + "\engine"
 $tempLocation = $tempDir + "\executable.zip"
 $installationDir = $env:ProgramFiles + "\engine"
-
-New-Item -ItemType Directory -Force -Path "$tempDir"
-
+$null = New-Item -ItemType Directory -Force -Path "$tempDir"
 $wc = New-Object net.webclient
 $wc.Downloadfile($executableUri, $tempLocation)
-
 Expand-Archive -Force -Path "$tempLocation" -DestinationPath "$installationDir"
-
 $pathContent = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine)
-
-$pathLine = ";$installationDir"
-
+$pathLine = ";$installationDir/engine"
 if (-not($pathContent -match [Regex]::Escape($pathLine))) {
     [Environment]::SetEnvironmentVariable("Path", $env:Path + $pathLine, [System.EnvironmentVariableTarget]::Machine)
 }
+echo "Installed DemaTrading.ai 'engine'."
+echo " Open a new powershell window and type 'engine init <YOUR DIRECTORY NAME>' to get started."
