0760abcdd571f8ad9c2682527893dfaded0dc5bc
Sherida van den Bent <sheridavandenbent@gmail.com>
2021-07-30 15:12:53 +0200

    Update test/setup/config/test_legacy_transforms.py

--- test/setup/config/test_legacy_transforms.py
+++ test/setup/config/test_legacy_transforms.py
@@ -1,7 +1,7 @@
 from modules.setup.config import transform_subplot_config
 
 
-def test_transform_subplot_config_v0_7_4):
+def test_transform_subplot_config_v0_7_4():
     result = transform_subplot_config(["indicator1", "indicator2"])
     assert result == [["indicator1"], ["indicator2"]]
 
