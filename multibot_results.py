7f572bb551735be587c8177a24f3fe04462944b3
marijn111 <39378193+marijn111@users.noreply.github.com>
2021-12-15 13:47:25 +0100

    add resulting processor

---
+++ multibot_results.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from glob import glob
+import os
+import json
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+files = glob(BASE_DIR + r"/data/backtesting-data/combined_*.json")
+
+with open(files[0], 'r') as f:
+    data = json.load(f)
+    df1 = pd.DataFrame(data).T
+
+with open(files[1], 'r') as f:
+    data = json.load(f)
+    df2 = pd.DataFrame(data).T
+
+with open(files[2], 'r') as f:
+    data = json.load(f)
+    df3 = pd.DataFrame(data).T
+
+
+df1 = df1.sort_values(by='drawdown', ascending=False)
+df2 = df2.sort_values(by='drawdown', ascending=False)
+df3 = df3.sort_values(by='drawdown', ascending=False)
+
+print(df1)
