fcf80684ad1617e12e342a3fa69182190ee2bb45
marijn111 <39378193+marijn111@users.noreply.github.com>
2021-12-16 18:11:11 +0100

    update result script

--- multibot_results.py
+++ multibot_results.py
@@ -2,30 +2,38 @@ import pandas as pd
 from glob import glob
 import os
 import json
+import copy
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 files = glob(BASE_DIR + r"/data/backtesting-data/combined_*.json")
 
-to_ignore = ['Eagle_heikin', 'Osprey_heikin', 'Osprey']
+to_ignore = ['Eagle_heikin', 'Osprey_heikin', 'Osprey-', '-Osprey']
+
+
+def return_cleaned_dict(data_dict):
+    data = {}
+    for key in data_dict.keys():
+        to_ignore_present = False
+        for ignore in to_ignore:
+            if ignore in key:
+                to_ignore_present = True
+        if not to_ignore_present:
+            data[key] = data_dict[key]
+        to_ignore_present = False
+    return pd.DataFrame(data).T
+
 
 with open(files[0], 'r') as f:
     data = json.load(f)
-    df1 = pd.DataFrame()
-    for bot in data:
-        for ignore in to_ignore:
-            if ignore in bot:
-                continue
-            df1.append({bot:data[bot]}, ignore_index=True)
-
-    df1 = df1.T
+    df1 = return_cleaned_dict(data)
 
 with open(files[1], 'r') as f:
     data = json.load(f)
-    df2 = pd.DataFrame(data).T
+    df2 = return_cleaned_dict(data)
 
 with open(files[2], 'r') as f:
     data = json.load(f)
-    df3 = pd.DataFrame(data).T
+    df3 = return_cleaned_dict(data)
 
 
 df1 = df1.sort_values(by='drawdown', ascending=False)
