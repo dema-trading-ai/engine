f6c7dc22d1af60e61eb7a9761d03d1516773fc11
Willem Gerritzen <willem.gerritzen@teampicnic.com>
2021-12-31 11:51:41 +0100

    Adapt conversion for tests

--- modules/stats/utils.py
+++ modules/stats/utils.py
@@ -10,14 +10,15 @@ def convert_dict_to_dataframe(per_timestamp_dict: dict, resample: bool = False) 
 
     df = pd.DataFrame.from_dict(per_timestamp_dict, columns=['capital'], orient='index')
 
-    first_value = df['capital'][0]
-    df = df.iloc[1:, :]  # Removing first row for the index to be transformed to Datetime correctly
+    if len(df['capital']) > 1:  # Don't run the conversion if the df has only one item
+        first_value = df['capital'][0]
+        df = df.iloc[1:, :]  # Removing first row for the index to be transformed to Datetime correctly
 
-    df.index = [datetime.fromtimestamp(date / 1000) for date in df.index]
+        df.index = [datetime.fromtimestamp(date / 1000) for date in df.index]
 
-    if resample:
-        df = df.resample('D').apply(lambda x: x.iloc[-1])
+        if resample:
+            df = df.resample('D').apply(lambda x: x.iloc[-1])
 
-    df.loc[df.index[0]] = first_value  # Replace with previous first value to include price of first buy in returns
+        df.loc[df.index[0]] = first_value  # Replace with previous first value to include price of first buy in returns
 
     return df
