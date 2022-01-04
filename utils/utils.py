bc47be29407d202e800cd427a4e34df729aebe52
Sherida van den Bent <sheridavandenbent@gmail.com>
2022-01-04 17:59:20 +0100

    update version number for release patch

--- utils/utils.py
+++ utils/utils.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from modules.stats.trade import Trade
 from cli.print_utils import print_config_error
 
-CURRENT_VERSION = "v0.7.13"
+CURRENT_VERSION = "v0.7.15"
 
 MILLISECONDS = 1000
 MINUTE = 60 * MILLISECONDS
