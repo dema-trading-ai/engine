71ef57f717707c1117703b795ac270b2e9d7cc61
Sherida van den Bent <sheridavandenbent@gmail.com>
2022-01-05 10:42:50 +0100

    remove optuna from maincontroller temporarily

--- main_controller.py
+++ main_controller.py
@@ -1,7 +1,7 @@
 # Files
 
-import optuna
-from optuna import Trial
+# import optuna
+# from optuna import Trial
 
 from backtest_runner import create_backtest_runner
 from cli.print_utils import print_info
@@ -13,13 +13,13 @@ class MainController:
     async def run(args) -> None:
         async with create_backtest_runner(args) as runner:
 
-            if args.alpha_hyperopt:
-                study = optuna.create_study()
-
-                def objective(trial: Trial):
-                    return runner.run_hyperopt_iteration(trial)
-
-                study.optimize(objective, n_trials=100)
-                print_info(f"Best results {study.best_params}")
-            else:
-                runner.run_outputted_backtest()
+            # if args.alpha_hyperopt:
+            #     study = optuna.create_study()
+            #
+            #     def objective(trial: Trial):
+            #         return runner.run_hyperopt_iteration(trial)
+            #
+            #     study.optimize(objective, n_trials=100)
+            #     print_info(f"Best results {study.best_params}")
+            # else:
+            runner.run_outputted_backtest()
