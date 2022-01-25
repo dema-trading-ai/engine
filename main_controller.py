1467d69d5b125fd503faca0912d7e7d2f842118b
Sherida van den Bent <sheridavandenbent@gmail.com>
2022-01-25 10:53:28 +0100

    add intermediate results and exit explanation to hyperopt

--- main_controller.py
+++ main_controller.py
@@ -1,5 +1,7 @@
 # Files
 import os
+import sys
+
 import optuna
 from optuna import Trial
 
@@ -22,10 +24,19 @@ class MainController:
                 else:
                     n_trials = 100
 
+                print_info(f"Running parameter hyperoptimization with {n_trials} trials.")
+                print_info("If you want to exit the program halfway, press 'ctrl + c', and you will get the "
+                           "intermediate results.")
+
                 def objective(trial: Trial):
                     return runner.run_hyperopt_iteration(trial)
 
-                study.optimize(objective, n_trials=n_trials)
-                print_info(f"Best results {study.best_params}")
+                try:
+                    study.optimize(objective, n_trials=n_trials)
+                except KeyboardInterrupt:
+                    print_info(f"Quitting hyperoptimization.")
+                    print_info(f"Best results: {study.best_params}")
+                    sys.exit()
+                print_info(f"Best results: {study.best_params}")
             else:
                 runner.run_outputted_backtest()
