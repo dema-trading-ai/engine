726dd8dd78f542a28e09742fa637014ace378056
Sherida van den Bent <sheridavandenbent@gmail.com>
2022-02-07 11:32:16 +0100

    test main build

--- main_controller.py
+++ main_controller.py
@@ -11,6 +11,7 @@ class MainController:
 
     @staticmethod
     async def run(args) -> None:
+        # add comment just for checking purpose
         async with create_backtest_runner(args) as runner:
 
             if args.alpha_hyperopt:
