f70fd1b44741edf102f62ecd23014968c2a30e3d
Sherida van den Bent <sheridavandenbent@gmail.com>
2022-02-21 12:05:47 +0100

    generating of data done

---
+++ cli/checks/engine_use_statistics.py
@@ -0,0 +1,40 @@
+import os
+import uuid
+
+from requests_futures.sessions import FuturesSession
+
+from cli.print_utils import print_warning
+from utils.utils import CURRENT_VERSION, is_running_as_executable
+
+
+def get_or_generate_random_id() -> str:
+    random_id = ""
+    create_new_id = True
+    if os.path.exists("./statistics_id.txt"):
+        create_new_id = False
+        with open("./statistics_id.txt", "r") as f:
+            data = f.read()
+            # check if data is valid uuid (protection for code injection)
+            try:
+                random_id = str(uuid.UUID(data))
+            except ValueError:
+                create_new_id = True
+    if create_new_id:
+        # create file and generate random id
+        random_id = str(uuid.uuid4())
+        with open("./statistics_id.txt", "w") as f:
+            f.write(random_id)
+    return random_id
+
+
+def engine_statistics(no_statistics: bool):
+    if no_statistics:
+        print_warning("In order to help DemaTrading.ai improve the Engine, please turn on the Engine statistics.")
+        return
+    session = FuturesSession()
+    stats = {
+        'random_id': get_or_generate_random_id(),
+        'engine_version': CURRENT_VERSION,
+        'executable': is_running_as_executable()
+    }
+    session.get('https://enginemetricspdbom7kn-logengineruns.functions.fnc.fr-par.scw.cloud')
--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import multiprocessing
 import os
 import sys
 from time import perf_counter
+from requests_futures.sessions import FuturesSession
 
 import optuna
 
 from cli.arg_parse import execute_for_args
+from cli.checks.engine_use_statistics import engine_statistics
 from cli.checks.latest_version import print_warning_if_version_outdated
 from cli.prepare_workspace import prepare_workspace
-from cli.print_utils import print_debug, is_verbosity
+from cli.print_utils import print_debug, is_verbosity, print_warning
 from main_controller import MainController
 from utils.utils import check_internet_connection
 
@@ -26,6 +28,9 @@ RUNFOLDER = os.path.dirname(os.path.realpath(__file__))
 def main(online: bool):
     if online:
         print_warning_if_version_outdated()
+    else:
+        print_warning("You are not connected to the internet. Certain functionality (like downloading new candle data)"
+                      " will not be available.")
     execute_for_args({
         'init': run_init,
         'default': run_engine
@@ -35,12 +40,17 @@ def main(online: bool):
 
 
 def run_engine(args, online: bool):
+    if online:
+        engine_statistics(args.no_statistics)
     controller = MainController()
     asyncio.run(controller.run(args, online))
 
 
 def run_init(args, _):
     prepare_workspace(args)
+
+
+
 
 
 if __name__ == "__main__":
