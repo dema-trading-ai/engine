9add2b4ff7efc3d60a9cc9f871f5278cb3ee9ece
WillemGerritzen <willemgerritzen@gmail.com>
2022-02-28 11:14:49 +0100

    [ENG-97] Test resources flag
    Signed-off-by: WillemGerritzen <willemgerritzen@gmail.com>

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from utils.utils import check_internet_connection, prepend_resource_dir
 
 # Hack, PyInstaller + rich on Windows in GitHub actions fails because it cannot find encoding of stdout, this sets
 # it on stdout if not set
+
 os.environ["PYTHONIOENCODING"] = "utf-8"
 PYTHONIOENCODING = os.environ.get("PYTHONIOENCODING", False)
 if sys.stdout.isatty() is False and PYTHONIOENCODING is not False and sys.stdout.encoding != PYTHONIOENCODING:
